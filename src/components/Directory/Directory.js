c7d70dc981dcb43abdf21ea6f5ceda80c885dc21
dev4 <dev4@newit-soft.ru>
2019-09-24 17:53:23 +0400

    Fixed inconsistent UI folder tree implementation

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -9,12 +9,12 @@ function Directory ({ directory, expandedDirectories, toggle, load }) {
   return (
     <div>
       <div className="d-flex">
-        <div onClick={() => toggle(directory.id)} className={'my-1 pr-2 d-flex align-items-center'}>
-          <span className={'Directory__caret-container mb-0 ' + (Object.keys(subNodes).length === 0 ? 'd-none' : '')}>
+        <div onClick={() => toggle(directory.id)} className={'pr-2 d-flex align-items-center'}>
+          <span className={'Directory__caret-container mb-0 ' + (Object.keys(subNodes).length === 0 ? 'invisible' : '')}>
             <i className={'Directory__caret text-info fas ' + (isExpanded ? 'fa-caret-down' : 'fa-caret-right')}/>
           </span>
         </div>
-        <div onClick={() => load(directory.id)} className="my-1 py-2 px-2 Directory__name">
+        <div onClick={() => load(directory.id)} className="py-2 pr-2 Directory__name">
           <i className={'mr-2 text-info fas fa-folder'}/>
           { directory.name }
         </div>
